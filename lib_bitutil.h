77a796c6935ea5abda6d82eacab041ed65a4f73c
Stefan Kos <stefan.kos@outlook.de>
2026-02-03 20:18:01 +0100

    Single and Multi Bit Manipulation Makros

--- lib_bitutil.h
+++ lib_bitutil.h
@@ -1,6 +1,44 @@
-#ifndef LIB_BITUTIL_   /* Include guard */
-#define LIB_BITUTIL_
+#ifndef LIB_BITUTIL_H  /* Include guard */
+#define LIB_BITUTIL_H
 
+//Include Section
+#include <stdint.h>
+#include <stddef.h>
 
+//Define Section
 
-#endif // LIB_BITUTIL_
+/* Makro Define Section */
+
+//Single Bit
+#define BIT_SET(reg,n)	( (reg) |= (1UL << (n)) )
+
+#define BIT_CLEAR(reg,n) ( (reg) &= ~(1UL <<(n)) )
+
+#define BIT_TOGGLE(reg, n) ( (reg) ^= (1UL << (n)) )
+
+#define BIT_READ(reg, n) ( (reg) & (1UL << (n)) )
+
+#define BIT_WRITE(reg, n, val) ( (val) ? BIT_SET(reg, n) : BIT_CLEAR(reg, n) )
+
+//Multi Bits
+#define BITS_SET(reg, mask) ( (reg) |= (mask) ) 
+
+#define BITS_CLEAR(reg, mask) ( (reg) &= ~(mask) )
+
+#define BITS_READ(reg, mask) ( (reg) & (mask) )
+
+#define BITS_WRITE(reg, mask, val) ( (reg) = ( (reg) & ~(mask) ) | ( (val) & (mask) ) ) 
+
+//BITFIELDS
+#define EXTRACT_BITFIELD(reg, hi, lo)
+
+#define INSERT_BITFIELD(reg, hi, lo, val)
+
+/*Function Define Section*/
+
+//BIT_POPCOUNT(reg)
+//BIT_REVERSE8(reg)
+//BIT_PARITY(reg)
+//BIT_FFS(reg)
+
+#endif // LIB_BITUTIL_H
